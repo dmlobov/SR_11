4743d9c7a54d44853a862f9e85b1f9d0a321004b
dmlob <dmlob@DESKTOP-0VEH751>
2022-11-26 13:31:35 +0300

    Добавьте файлы проекта.

---
+++ Task1/Linurav.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Linurav
+    {
+        double k;
+        double b;
+
+        public Linurav(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            return $"Решение уравнения х = {x}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение коэф к уравнения 0=kx+b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение коэф b уравнения 0=kx+b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linurav linurav = new Linurav(k, b);
+            Console.WriteLine(linurav.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
